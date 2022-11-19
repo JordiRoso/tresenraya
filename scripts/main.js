e98cf60921c50084a74f2935292ca51c2950978b
Jordi <jordirosado@jordirosado.com>
2022-11-19 09:43:07 +0100

    borrado de linesa inecesarias

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ console.log( 'jugadorx', esJugadorX)
 
 
 const playerClick = (ev)=> {
-   // console.log(ev.target.innerHtml)
+   
 
 let namex = document.getElementById("jugador1_start").value;
 let nameo = document.getElementById("jugador2_start").value;
@@ -40,8 +40,7 @@ let box31Value = document.getElementById('box31') .innerHTML
 let box32Value = document.getElementById('box32') .innerHTML
 let box33Value = document.getElementById('box33') .innerHTML
 
-//let namex = document.getElementById("jugador1_start").value;
-//let nameo = document.getElementById("jugador2_start").value;
+
 
 let uiBox11 = document.getElementById ('box11')
 let uiBox12 = document.getElementById ('box12')
@@ -75,18 +74,7 @@ let uiBox33 = document.getElementById ('box33')
         turnox.innerHTML = ""
 
 
-        //box11Value = document.getElementById('box11') .innerHTML = "🏆"
-        //box12Value = document.getElementById('box12') .innerHTML = "🏆"
-        //box13Value = document.getElementById('box13') .innerHTML = "🏆"
-
-        //box21Value = document.getElementById('box21') .innerHTML = "w"
-        //box22Value = document.getElementById('box22') .innerHTML = "i"
-       // box23Value = document.getElementById('box23') .innerHTML = "n"
-
-       // box31Value = document.getElementById('box31') .innerHTML = "🏆"
-        //box32Value = document.getElementById('box32') .innerHTML = "🏆"
-       // box33Value = document.getElementById('box33') .innerHTML = "🏆"
-        //window.alert("que pasa neng!!!");
+        
     }
        if ((box21Value == 'X') && (box22Value  == 'X') && ( box23Value == 'X')) {
             document.getElementById('imprimir').innerHTML = "La x gana, a Jesús nadie le chulea";
@@ -286,25 +274,7 @@ let uiBox33 = document.getElementById ('box33')
 
 
 
-//const playerClick = (ev)=> {
-    //console.log(ev.target.innerHtml)
- 
- 
- 
- //let imprimir = ev.target
- //let box11Value = ev.target.innerHTML
- //let box12Value = ev.target.innerHTML
- //let box13Value = ev.target.innerHTML
-     
- 
-     //if (box11Value == "x")(box12Value == "x") (box13Value == "x")
-    // {
-         
-             //imprimir.innerHTML = 'ganador x'
-             
-        // }
-         
-    // }
+
  
  
 
@@ -318,14 +288,7 @@ let uiBox33 = document.getElementById ('box33')
 
 
 
-    //let boxes = document. getElementsByClassName('box')
-  //for (let i = 0; i < boxes.length; i++) {
-    //const box = boxes [i];
-     // box.addEventListener('click', (ev) => {
-      // playerClick (ev)
-   // })
-//}
-
+  
    
 
 
